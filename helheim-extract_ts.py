85c04771004a83cba16430d49e0f414a6645da28
Lizz Ultee [air] <ehultee@umich.edu>
2020-05-07 17:34:27 -0400

    Improve plotting of tributary timeseries
    Use a nice colormap, plot colour-coded locations of the series extracted, simplify ticks.

--- helheim-extract_ts.py
+++ helheim-extract_ts.py
@@ -2,6 +2,7 @@
 ## 19 Mar 2020  EHU
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib import cm
 import iceutils as ice
 import sys
 
@@ -22,16 +23,25 @@ series = [hel_stack.timeseries(xy=xyi, key=data_key) for xyi in xys]
 
 s = hel_stack.slice(index=10, key=data_key)
 
+## Make some plots to show at EGU
+colors = cm.get_cmap('Dark2')(np.linspace(0, 1, num=len(labels)))
 fig1, ax1 = plt.subplots()
 for i, ser in enumerate(series):
-	ax1.plot(hel_stack.tdec, ser, marker='o', lw=0.5, label=labels[i])
-ax1.set_ylabel('Speed [km/a]')
+	ax1.plot(hel_stack.tdec, ser, marker='o', lw=0.5, label=labels[i], color=colors[i])
+ax1.set(xlabel='Year', ylabel='Speed [km/a]')
 ax1.legend(loc='best')
 plt.show()
 
 
 fig2, ax2 = plt.subplots()
-ax2.contourf(hel_stack.stacks[0]._datasets['x'], hel_stack.stacks[0]._datasets['y'], s)
-for xy in (xy_1, xy_2, xy_3, xy_4):
-	ax2.plot(xy[0], xy[1], marker='*')
+ax2.contourf(hel_stack.stacks[0]._datasets['x'], hel_stack.stacks[0]._datasets['y'], s, 50)
+for i, xy in enumerate(xys):
+	ax2.plot(xy[0], xy[1], marker='*', markersize=10, color=colors[i])
+ax2.set(
+	xlim=(292000, 320500), ylim=(-2581400, -2555000),
+	xticks=[295000, 300000, 305000, 310000, 315000, 320000], xticklabels=['295', '300', '305', '310', '315', '320'], xlabel='Easting [km]',
+	yticks=[-2580000, -2575000, -2570000, -2565000, -2560000, -2555000], yticklabels=['-2580', '-2575', '-2570', '-2565', '-2560', '-2555'], ylabel='Northing [km]'
+	)
+ax2.set_aspect(1)
+plt.tight_layout()
 plt.show()
